17f2c59be1529ba2c902ded9867f6bab9bb0e89e
Josh Goodman <jogoodma@iu.edu>
2019-11-26 12:29:05 -0500

    Started work on adding bootstrap buttons.

--- src/components/LayoutControl/index.js
+++ src/components/LayoutControl/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import ToggleButton from 'react-bootstrap/lib/ToggleButton'
+import ToggleButtonGroup from 'react-bootstrap/lib/ToggleButtonGroup'
 
 const LayoutControl = ({ handleOnClick = () => {} }) => (
   <div>
+    <ButtonToolbar>
+      <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+        <ToggleButton value={1}>Radio 1 (pre-checked)</ToggleButton>
+        <ToggleButton value={2}>Radio 2</ToggleButton>
+        <ToggleButton value={3}>Radio 3</ToggleButton>
+      </ToggleButtonGroup>
+    </ButtonToolbar>
     <button onClick={() => handleOnClick('functional')}>Functional</button>
     <button onClick={() => handleOnClick('pathway')}>Pathway</button>
   </div>
